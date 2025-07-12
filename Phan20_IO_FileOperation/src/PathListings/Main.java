6d245bf045798a49d1424f40eb67920b13d70040
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-07-12 09:09:38 +0700

    IO NIO NIO2

---
+++ Phan20_IO_FileOperation/src/PathListings/Main.java
@@ -0,0 +1,26 @@
+package PathListings;
+
+import java.nio.file.Path;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Path path = Path.of("files/testing.txt");
+        printPathInfo(path);
+
+    }
+
+    private static void printPathInfo(Path path) {
+
+        System.out.println("Path: " + path);
+        System.out.println("fileName = " + path.getFileName());
+        System.out.println("parent = " + path.getParent());
+        Path absolutePath = path.toAbsolutePath();
+        System.out.println("Absolute Path: = " + absolutePath);
+        System.out.println("Absolute Path Root: = " + absolutePath.getRoot());
+        System.out.println("isAbsolute = " + path.isAbsolute());
+        System.out.println("-----------------------");
+
+    }
+}
