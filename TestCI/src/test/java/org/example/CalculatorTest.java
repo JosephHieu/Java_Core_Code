06d1796eb630482225fefa31f1b61f3b9f8c2b7e
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-24 21:02:47 +0700

    Test Sub

--- TestCI/src/test/java/org/example/CalculatorTest.java
+++ TestCI/src/test/java/org/example/CalculatorTest.java
@@ -1,16 +1,27 @@
 package org.example;
 
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    Calculator calc;
+    @BeforeEach
+    public void setup() {
+         calc = new Calculator();
+
+    }
 
     @Test
     public void testAdd() {
 
-        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3), "Should be 5");
+    }
 
-        Assertions.assertEquals(5, calc.add(2, 3), "Should be 5");
+    @Test
+    public void testSub() {
+        assertEquals(10, calc.sub(20, 10));
     }
 }
